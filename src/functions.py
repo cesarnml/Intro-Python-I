ab42783431fef9bc80b3665a0b693682b3d184c4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-12 15:13:16 -0800

    completes function.py; 11/16

--- src/functions.py
+++ src/functions.py
@@ -6,7 +6,12 @@
 num = input("Enter a number: ")
 num = int(num)
 
+if (num % 2 == 0): 
+    print(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+if (num % 2 == 0):
+    print('Even!')
+else:
+    print('Odd')
