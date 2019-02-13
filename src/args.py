c206bb9eb9ede0ddc2ed46b12f59e8ac14e620c5
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-12 17:19:16 -0800

    completes args.pv; fixes an input by employing variable abstraction instead of hardcoded input; 12/16

--- src/args.py
+++ src/args.py
@@ -14,58 +14,64 @@ print(f1(1,2))
 
 # YOUR CODE HERE
 # x = input("Enter a comma seperated list of integers: ").split(',')
-def f2(x):
-    sum = 0
-    for i in x:
-        sum += i
-    return sum
+def f2(*x):
+    return sum(x)
 
 
 print(f2(1))                    # Should print 1
-# print(f2(1, 3))                 # Should print 4
-# print(f2(1, 4, -12))            # Should print -7
-# print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+print(f2(1, 3))                 # Should print 4
+print(f2(1, 4, -12))            # Should print -7
+print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-# a = [7, 6, 5, 4]
+a = [7, 6, 5, 4,]
+
+# What thing do you have to add to make this work?
+print(f2(*a))    # Should print 22
+
+# Write a function f3 that accepts either one or two arguments. If one argument,
+# it returns that value plus 1. If two arguments, it returns the sum of the
+# arguments. Google "python default arguments" for a hint.
+
+# YOUR CODE HERE
+
+def f3(*args):
+    if len(args) == 1: 
+        return args[0] + 1
+    elif len(args) == 2:
+        return sum(args)
+
+print(f3(1, 2))  # Should print 3
+print(f3(8))     # Should print 9
+
+
+# Write a function f4 that accepts an arbitrary number of keyword arguments and
+# prints ouf the keys and values like so:
+#
+# key: foo, value: bar
+# key: baz, value: 12
+#
+# Google "python keyword arguments".
+
+# YOUR CODE HERE
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {k:10s}, value: {v}')
+
+# Should print
+# key: a, value: 12
+# key: b, value: 30
+f4(a=12, b=30)
+
+# Should print
+# key: city, value: Berkeley
+# key: population, value: 121240
+# key: founded, value: "March 23, 1868"
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
+
+d = {
+    "monster": "goblin",
+    "hp": 3,
+}
 
 # # What thing do you have to add to make this work?
-# print(f2(a))    # Should print 22
-
-# # Write a function f3 that accepts either one or two arguments. If one argument,
-# # it returns that value plus 1. If two arguments, it returns the sum of the
-# # arguments. Google "python default arguments" for a hint.
-
-# # YOUR CODE HERE
-
-# print(f3(1, 2))  # Should print 3
-# print(f3(8))     # Should print 9
-
-
-# # Write a function f4 that accepts an arbitrary number of keyword arguments and
-# # prints ouf the keys and values like so:
-# #
-# # key: foo, value: bar
-# # key: baz, value: 12
-# #
-# # Google "python keyword arguments".
-
-# # YOUR CODE HERE
-
-# # Should print
-# # key: a, value: 12
-# # key: b, value: 30
-# f4(a=12, b=30)
-
-# # Should print
-# # key: city, value: Berkeley
-# # key: population, value: 121240
-# # key: founded, value: "March 23, 1868"
-# f4(city="Berkeley", population=121240, founded="March 23, 1868")
-
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
-
-# # What thing do you have to add to make this work?
-# f4(d)
+f4(**d)
